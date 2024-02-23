497a81bf06be73609bdffd70654ae7d7e3f6d91e
Md Kawsar Islam Yeasin <87494463+yeasin-2002@users.noreply.github.com>
2024-02-23 13:41:51 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="grid min-h-screen place-items-center bg-gray-900 text-white ">
+      <h1 className="text-center text-4xl font-bold">
+        React TypeScript TailwindCSS Templates with Vite
+      </h1>
+      <p className="text-center text-2xl">
+        Md Kawsar Islam Yeasin (@yeasin2002)
+      </p>
+    </div>
+  );
+};
+
+export default App;
